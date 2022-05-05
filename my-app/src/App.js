472cf524d013f843e65f7c392979de91d2280ea0
adi-odw <aditya@qtoss.in>
2022-05-05 21:24:32 +0530

    vicki first commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React vicki hello jhbuyjv sdgds gsg 
         </a>
       </header>
     </div>
